4581c70b3d4d500c2acb4c0bc58414c4a07d52cd
drfailer <drfailer.42@gmail.com>
2025-03-05 14:15:17 -0500

    fix: missing types files

---
+++ src/types.hpp
@@ -0,0 +1,9 @@
+#ifndef TYPES_H
+#define TYPES_H
+#include <vector>
+
+using Vector = std::vector<double>;
+using Vectors = std::vector<std::vector<double>>;
+using DataBase = std::vector<std::pair<Vector, Vector>>;
+
+#endif
